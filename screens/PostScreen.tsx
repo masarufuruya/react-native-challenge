4bb67fa642525ac6e0a188992352c3352983042d
masarufuruya <masarufuruya116@gmail.com>
2020-11-13 07:05:55 +0900

    image picker導入

--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Text, StyleSheet } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import React, { useEffect, useState } from 'react'
+import { Text, StyleSheet, Platform } from 'react-native'
 import {
   Button,
   Container,
@@ -40,10 +41,37 @@ const PostScreen = (props) => {
     collectionsStore,
   } = props
 
+  useEffect(() => {
+    (async () => {
+      //カメラロールの許可を取得する
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
   const resetForm = () => {
     setName("")
     setDescription("")
   }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      // setImage(result.uri);
+    }
+  };
 
   const onPressSaveButton = () => {
     if (name === "" || description == "") return
@@ -64,6 +92,11 @@ const PostScreen = (props) => {
       </Header>
       <Content padder>
         <Form>
+          <Button
+            onPress={() => pickImage()}
+          >
+            <Text>アップロード</Text>
+          </Button>
           <Item regular style={styles.name}>
             <Input
               placeholder='タイトル'
