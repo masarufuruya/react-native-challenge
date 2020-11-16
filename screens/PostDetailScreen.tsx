0dc2bde090d5befc40431a69e086045cdbcdd64c
masarufuruya <masarufuruya116@gmail.com>
2020-11-16 09:31:36 +0900

    詳細画面のスタイル調整

--- screens/PostDetailScreen.tsx
+++ screens/PostDetailScreen.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+} from 'react-native'
 import { useRoute } from '@react-navigation/native';
 
 const PostDetailScreen = () => {
@@ -11,10 +17,32 @@ const PostDetailScreen = () => {
 
   return (
     <View>
-      <Text>{collection.name}</Text>
-      <Text>{collection.description}</Text>
+      <Image
+        source={{
+          uri: collection.photo
+        }}
+        style={styles.photo}
+      />
+      <ScrollView style={styles.descriptionContainer}>
+        <Text>{collection.description}</Text>
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  descriptionContainer: {
+    backgroundColor: "#fff",
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 20,
+    height: 400,
+  },
+  photo: {
+    width: "100%",
+    height: 200,
+  },
+});
 
 export default PostDetailScreen
