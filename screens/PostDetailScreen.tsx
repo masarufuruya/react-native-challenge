738768a5cf26827eee083c60d947a396821f7853
masarufuruya <masarufuruya116@gmail.com>
2020-11-17 07:26:56 +0900

    スキボタンと編集ボタンを追加

--- screens/PostDetailScreen.tsx
+++ screens/PostDetailScreen.tsx
@@ -6,7 +6,11 @@ import {
   StyleSheet,
   ScrollView,
 } from 'react-native'
-import { useRoute } from '@react-navigation/native';
+import {
+  Button,
+} from 'native-base'
+import { FontAwesome } from '@expo/vector-icons'
+import { useRoute } from '@react-navigation/native'
 
 const PostDetailScreen = () => {
   const route = useRoute();
@@ -23,6 +27,16 @@ const PostDetailScreen = () => {
         }}
         style={styles.photo}
       />
+      <View style={styles.buttonContainer}>
+        <Button bordered warning style={styles.likeButton}>
+          <FontAwesome style={styles.likeIcon} name="heart" size={17} color="#EB9D3D" />
+          <Text>スキ 0</Text>
+        </Button>
+        <Button bordered warning style={styles.editButton}>
+          <FontAwesome style={styles.editIcon} name="edit" size={24} color="#EB9D3D" />
+          <Text>書く</Text>
+        </Button>
+      </View>
       <ScrollView style={styles.descriptionContainer}>
         <Text>{collection.description}</Text>
       </ScrollView>
@@ -42,6 +56,29 @@ const styles = StyleSheet.create({
   photo: {
     width: "100%",
     height: 200,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  likeButton: {
+    width: 120,
+    justifyContent: 'center',
+    marginRight: 15
+  },
+  likeIcon: {
+    marginRight: 5,
+    marginTop: 1
+  },
+  editButton: {
+    width: 120,
+    justifyContent: 'center',
+  },
+  editIcon: {
+    marginRight: 5,
+    marginTop: 1
   },
 });
 
